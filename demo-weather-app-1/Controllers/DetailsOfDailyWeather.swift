a1928585902589c5f81afd04d52e4bb36f928a1f
Abdullah Mohammad Daihan <abdullahmohammaddaihan@Abdullahs-Mac-mini.local>
2021-08-13 16:40:48 +0600

    testing push 3

--- demo-weather-app-1/Controllers/DetailsOfDailyWeather.swift
+++ demo-weather-app-1/Controllers/DetailsOfDailyWeather.swift
@@ -26,6 +26,7 @@ class DetailsOfDailyWeatherViewController: UIViewController {
     var theDate:String?
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("-")
         
     }
 
